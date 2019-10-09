f3e6b92db29f4aef2ce2160f86ff40e5c86e8bc8
Daniel G Encina A <ext_dgencina@falabella.cl>
2019-10-09 11:08:33 -0300

    seccion 5 pipes

---
+++ 03-pipes/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = 'Daniel';
+  nombre2 = 'daniel gonzalo encina acosta';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: '500',
+    direccion: {
+      calle: 'Primera',
+      casa: '12'
+    }
+  };
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Llego la data :::');
+    }, 3500);
+  });
+
+  date = new Date();
+
+  video = 'qRp3-D3SMwI';
+
+  activar = true;
+}
---
+++ 03-pipes/src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'contrasena'
+})
+export class ContrasenaPipe implements PipeTransform {
+
+  transform(value: any, activar: boolean = true): string {
+    return activar ? value.replace(/./g, '*') : value;
+  }
+}
